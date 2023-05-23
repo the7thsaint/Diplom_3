be24c3d64a9b76c50117782e1eab8447615f5d3f
Pavel Vinogradov <pav.vinogradow1@yandex.ru>
2023-05-23 11:57:01 +0300

    edit pom add UserSteps

---
+++ src/test/java/org/example/UserInfo/UserSteps.java
@@ -0,0 +1,28 @@
+package org.example.UserInfo;
+
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import org.example.pageObject.Constants;
+
+import static io.restassured.RestAssured.given;
+
+public class UserSteps {
+    @Step("Создание нового пользователя")
+    public Response createNewUser(UserInfo userInfo){
+        return given()
+                .header("Content-Type", "application/json")
+                .body(userInfo)
+                .post(Constants.REGISTER);
+    }
+
+    @Step("Логин пользователя")
+    public Response loginUser(UserInfo userInfo){
+        return given()
+                .header("Content-Type", "application/json")
+                .body(userInfo)
+                .post(Constants.LOGIN);
+    }
+
+
+}
--- src/test/java/org/example/pageObject/Constants.java
+++ src/test/java/org/example/pageObject/Constants.java
@@ -2,4 +2,7 @@ package org.example.pageObject;
 
 public class Constants {
     public static final String BURGER_MAIN_PAGE = "https://stellarburgers.nomoreparties.site";
+    public static final String LOGIN = "api/auth/login";
+    public static final String REGISTER = "api/auth/register";
+
 }

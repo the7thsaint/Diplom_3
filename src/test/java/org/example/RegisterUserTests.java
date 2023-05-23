6ce761fa0e8ad6a462ebcde38d0503a151f4fc47
Pavel Vinogradov <pav.vinogradow1@yandex.ru>
2023-05-23 21:14:40 +0300

    Finish register tests

--- src/test/java/org/example/RegisterUserTests.java
+++ src/test/java/org/example/RegisterUserTests.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class RegisterUserTests {
     public WebDriver driver;
@@ -50,7 +51,18 @@ public class RegisterUserTests {
     public void testSuccessRegister(){
         driver.get(Constants.BURGER_MAIN_PAGE+Constants.REGISTER_PAGE);
         registrationPage.insertUserDataAndClick(userInfo);
-        assertEquals("Переход на страницу логина", Constants.BURGER_MAIN_PAGE+Constants.LOGIN_PAGE);
+        assertEquals("Переход на страницу логина", Constants.BURGER_MAIN_PAGE+Constants.LOGIN_PAGE,driver.getCurrentUrl());
+    }
+    //Фэйкер перестал выдавать уникальные значение, надо подумать как зарандомить еще
+
+    @Test
+    @DisplayName("Проверка вывода напдиси о некорректном пароле")
+    public void testErrorAfterWrongData(){
+        userInfo.setPassword("12345");
+        driver.get(Constants.BURGER_MAIN_PAGE+Constants.REGISTER_PAGE);
+        registrationPage.insertUserDataAndClick(userInfo);
+        assertTrue("Ошибка о некорректном пароле отображается", registrationPage.checkWrongPasswordMessage());
+        assertEquals("Пользователь остался на странице с регистрацией", Constants.BURGER_MAIN_PAGE+Constants.REGISTER_PAGE,driver.getCurrentUrl());
     }
 
 
